3a50cc2cc337c8e52d8bd5181193cc80d159e598
gilcierweb <gilcier06@yahoo.com.br>
2019-02-06 01:50:16 -0300

    improve form submit

--- form_contact.py
+++ form_contact.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, TextAreaField
+from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 csrf = CSRFProtect()
@@ -9,3 +9,4 @@ class ContactForm(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired('E-mail não pode ficar vazio'),Email('Informe um email válido')])
     subject = StringField('Assunto', validators=[DataRequired('Assunto não pode ficar vazio')])
     message = TextAreaField('Mensagem', validators=[DataRequired('Mensagem não pode ficar vazio')])
+    submit = SubmitField("Enviar")
